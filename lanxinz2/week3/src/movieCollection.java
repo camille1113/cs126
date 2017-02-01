49333082dacea67e840130f2afdee0c3347d1a30
lanxinz2 <lanxinz2@dab2f74c-00ed-4fb6-9d07-09b998441997>
2017-02-01 03:49:25 +0000

    MovieParser

---
+++ lanxinz2/week3/src/movieCollection.java
@@ -0,0 +1,77 @@
+/**
+ * Created by zhanglanxin on 1/30/17.
+ */
+public class movieCollection {
+    public static void main(String[] args){
+
+    }
+    private int page;
+    private movie[] results;
+    private int total_results;
+    private int total_pages;
+
+    public movie[] getResult() {
+        return results;
+    }
+
+    public int getPage() {
+
+        return page;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public int getTotal_results() {
+
+        return total_results;
+    }
+
+
+
+    public String allMovieTitles(){
+        String MovieTitle = " ";
+        for(int i = 0; i < results.length; i++){
+            MovieTitle += results[i].getTitle() + "\n";
+        }
+        System.out.println(MovieTitle);
+        return MovieTitle;
+    }
+
+    public String genre14(){
+        String genre14MovieTitle = " ";
+        for(int i = 0; i < results.length; i++){
+            if(results[i].genre()) {
+                genre14MovieTitle += results[i].getTitle() + "\n";
+            }
+        }
+        System.out.println(genre14MovieTitle);
+        return genre14MovieTitle;
+    }
+    public String vote5(){
+        String vote5MovieTitle = " ";
+        for(int i = 0; i < results.length; i++){
+            if(results[i].voteAve()) {
+                vote5MovieTitle += results[i].getTitle() + "\n";
+            }
+        }
+        System.out.println(vote5MovieTitle);
+        return vote5MovieTitle;
+    }
+    public String pop1(){
+        String pop1MovieTitle = " ";
+        for(int i = 0; i < results.length; i++){
+            if(results[i].pop()) {
+                pop1MovieTitle += results[i].getTitle() + "\n";
+            }
+        }
+        System.out.println(pop1MovieTitle);
+        return pop1MovieTitle;
+    }
+
+
+
+
+
+}
