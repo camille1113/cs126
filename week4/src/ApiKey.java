51c94b871971a3a9138a628230430cc0f5725eb8
lanxinz2 <lanxinz2@dab2f74c-00ed-4fb6-9d07-09b998441997>
2017-02-08 03:22:30 +0000

    MovieParser

--- week4/src/ApiKey.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by zhanglanxin on 2/7/17.
- */
-public class ApiKey {
-    public static final String API_KEY = "https://api.themoviedb.org/3/movie/popular?api_key=bf6f2a540bd3be3ba9707da5e6440faf";
-}
