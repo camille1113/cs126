368e89ec711bfead972f35d7f0e23d8c7579cf7d
lanxinz2 <lanxinz2@dab2f74c-00ed-4fb6-9d07-09b998441997>
2017-02-15 02:21:16 +0000

    zork

---
+++ week5/src/Direction.java
@@ -0,0 +1,20 @@
+/**
+ * Created by zhanglanxin on 2/12/17.
+ */
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Direction {
+
+    private String direction;
+    private String room;
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+}
---
+++ week5/src/Layout.java
@@ -0,0 +1,21 @@
+/**
+ * Created by zhanglanxin on 2/12/17.
+ */
+import java.util.List;
+
+
+public class Layout {
+
+    private String initialRoom;
+    private List<Room> rooms = null;
+
+    public String getInitialRoom() {
+        return initialRoom;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+
+}
